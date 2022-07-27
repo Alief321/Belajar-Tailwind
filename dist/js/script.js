fa542fde39e555b757170c61ab65b51c77569c64
Alief Zulvan <depanb187@gmail.com>
2022-07-27 09:44:34 +0700

    tambahin fitur dikit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 // Hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#navMenu');
+const totop = document.querySelector('#totop');
 
 hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
@@ -14,7 +15,40 @@ window.onscroll = function () {
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add('navbar-fix');
+    totop.classList.remove('hidden');
+    totop.classList.add('flex');
   } else {
     header.classList.remove('navbar-fix');
+    totop.classList.add('hidden');
+    totop.classList.remove('flex');
   }
 };
+
+// click luar
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// darkmode toggle
+const darkToggle = document.querySelector('#darkmode');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// pindah toglle mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
